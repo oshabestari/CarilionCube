e678dbf7b09ab96bf59dc6339c7e0af34c6c8210
Carlos <CarlosSolrac@users.noreply.github.com>
2018-05-30 14:47:45 -0700

    Parse a SQL file and convert single quotes to four quotes for use in openquery. Also pretty print the SQL code.

---
+++ QuerySchemas/ConvertSingleQuoteToDoubleQuote.py
@@ -0,0 +1,36 @@
+
+import html
+import re
+import sqlparse
+import format_sql as fsql
+
+with open('./queries.txt', 'r') as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+content = [x.strip() for x in content]
+
+
+with open('./queries.sql', 'w') as f:
+    for l in content:
+        if 'set @xml=' in l:
+            l = l.replace("set @xml='", '')
+            l = l[:-1]
+            l = html.unescape(l)
+
+            print(l)
+            fmt = sqlparse.format(l, reindent=True)
+            if (fmt is not None):
+               l = fmt
+            
+            # fmt = fsql.format_sql(l, debug=True)
+            # if (fmt is not None):
+            #     l = '\n'.join(fmt)
+            
+            l = l.replace("'", "''''")
+            
+            l = "set @xml='" + l + "'"
+            # m = re.findall(r'from\b.*', l, re.IGNORECASE)
+            # for mm in m:
+            #     print(mm)
+        f.write(l + '\n')
+
