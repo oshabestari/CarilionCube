d61f1390454d787bea2930c4cb190ef520635b48
Carlos <CarlosSolrac@users.noreply.github.com>
2018-07-05 12:26:05 -0700

    Make SqlCmd run then backup and then quit.

--- SqlCodeBackup/ScriptSqlServer_ScriptOut_localhost_carilion_dw.cmd
+++ SqlCodeBackup/ScriptSqlServer_ScriptOut_localhost_carilion_dw.cmd
@@ -1,2 +1,2 @@
 powershell -NonInteractive -file C:\_car\CarilionCube\SqlCodeBackup\PowerShellScriptServer_Template.ps1 -sql_server localhost
-sqlcmd -S localhost -E -d carilion_dw -q "exec [dbo].[usp_BackupDatabase]"
+sqlcmd -S localhost -E -d carilion_dw -Q "exec [dbo].[usp_BackupDatabase]"
