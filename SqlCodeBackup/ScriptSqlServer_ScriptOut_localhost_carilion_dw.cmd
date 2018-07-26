b6ca51fc13a95694f234e1db974aebe1888c2403
Carlos <CarlosSolrac@users.noreply.github.com>
2018-07-25 18:30:46 -0700

    Backup databse and compress using 7zip

--- SqlCodeBackup/ScriptSqlServer_ScriptOut_localhost_carilion_dw.cmd
+++ SqlCodeBackup/ScriptSqlServer_ScriptOut_localhost_carilion_dw.cmd
@@ -1,3 +1,6 @@
 powershell -NonInteractive -file C:\_car\CarilionCube\SqlCodeBackup\PowerShellScriptServer_Template.ps1 -sql_server localhost\sql2016
 sqlcmd -S localhost\sql2016 -E -d CubeMetaData -Q "exec [dbo].[usp_BackupDatabase_Carlos]"
 sqlcmd -S localhost\sql2016 -E -d carilion_dw -Q "exec [dbo].[usp_BackupDatabase_Carlos]"
+
+del C:\_car\CarilionCube\DW_backup\CubeMetaData.bak.7z
+"c:\Program Files\7-Zip\7z.exe" a -t7z -m0=PPMd -mx=9 "C:\_car\CarilionCube\DW_backup\CubeMetaData.bak.7z" "C:\_car\CarilionCube\DW_backup\CubeMetaData.bak"
