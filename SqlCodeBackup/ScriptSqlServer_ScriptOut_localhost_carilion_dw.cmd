bacad8dba546c5ddca7c5b8b79198bf68e983aac
Carlos Klapp <CarlosSolrac@users.noreply.github.com>
2018-08-01 22:53:21 -0700

    Script out caubeMetaData_stj

--- SqlCodeBackup/ScriptSqlServer_ScriptOut_localhost_carilion_dw.cmd
+++ SqlCodeBackup/ScriptSqlServer_ScriptOut_localhost_carilion_dw.cmd
@@ -1,5 +1,6 @@
 powershell -NonInteractive -file C:\_car\CarilionCube\SqlCodeBackup\PowerShellScriptServer_Template.ps1 -sql_server localhost\sql2016
 sqlcmd -S localhost\sql2016 -E -d CubeMetaData -Q "exec [dbo].[usp_BackupDatabase_Carlos]"
+sqlcmd -S localhost\sql2016 -E -d CubeMetaData_stj -Q "exec [dbo].[usp_BackupDatabase_Carlos]"
 sqlcmd -S localhost\sql2016 -E -d carilion_dw -Q "exec [dbo].[usp_BackupDatabase_Carlos]"
 
 del /Q C:\_car\CarilionCube\DW_backup\CubeMetaData.bak.7z
